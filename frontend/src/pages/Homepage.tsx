5c3e1ee849e79633e975affe93df1aabb3fc3bfa
Oliver Ladores <oliver.ladores@wvsu.edu.ph>
2024-11-16 02:56:19 +0800

    Fixed conflicts from testing code earlier.

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -1,12 +1,20 @@
-import React from "npm:react";
-import Nav from "../components/Nav.tsx";
-const Homepage = () => {
-  return (
-    <div>
-      <Nav />
-      <h1 className="font-leader font-bold text-red-500 text-8xl">Homepage!</h1>
+import React from "npm:react"
+import Nav from "../components/Nav.tsx"
+const Homepage = () => (
+  <>
+    <Nav />
+    {/* greeter screen */}
+    <div className="h-screen flex flex-wrap content-center">
+      <div className="max-h-[40dvh] w-screen px-52 border-4 border-green-400">
+        <h1 className="text-6xl text-center font-leader uppercase">
+          Welcome To Your One-Stop Org Hub
+        </h1>
+        <p>
+
+        </p>
+      </div>
     </div>
-  );
-};
+  </>
+);
 
 export default Homepage;
