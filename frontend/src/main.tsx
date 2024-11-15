7d2d71844f6dccd265c688609aba3d82a67585ae
Oliver Ladores <oliver.ladores@wvsu.edu.ph>
2024-11-15 14:22:33 +0800

    Added routing with `react-router-dom`.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,44 @@
+/// <reference lib="dom" />
+
 import React from "npm:react"
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider
+} from "react-router-dom"
 import "./index.css";
-import App from "./App.tsx";
+import Homepage from "./pages/Homepage.tsx";
+import AboutUs from "./pages/AboutUs.tsx";
+import ContactsUs from "./pages/ContactsUs.tsx";
+import Events from "./pages/Events.tsx";
+import Organizations from "./pages/Organizations.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />,
+  },
+  {
+    path: "about",
+    element: <AboutUs />,
+  },
+  {
+    path: "contact",
+    element: <ContactsUs />,
+  },
+  {
+    path: "events",
+    element: <Events />,
+  },
+  {
+    path: "orgs",
+    element: <Organizations />,
+  },
+])
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
