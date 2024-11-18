8f7a1c763fc4c82da8c1bc5eea6baa9103708b78
Cristopher-Artacho-WVSU <cristopherian.artacho@wvsu.edu.ph>
2024-11-18 21:50:43 +0800

    Added changes, made branch

--- frontend/src/pages/AboutUs.tsx
+++ frontend/src/pages/AboutUs.tsx
@@ -7,7 +7,7 @@ const AboutUs = () => {
       {/* LAYER 1 */}
       <div className="h-screen bg-white relative">
         {/* OVAL */}
-        <div className="absolute left-1/2 -translate-x-1/2 w-[150%] h-[100vh] bg-[#FF6C1F] rounded-[80%_80%] top-[-50vh]"></div>
+        <div className="absolute left-1/2 -translate-x-1/2 w-[150%] h-[100vh] bg-[#FF6C1F] rounded-[80%_80%] top-[-50%]"></div>
         {/* ABOUT WVSU TEXT */}
         <div className="font-leader z-10 text-center absolute left-1/2 -translate-x-1/2 top-[15%] text-[80px] whitespace-nowrap">
           ABOUT WVSU ORBIT
@@ -21,8 +21,45 @@ const AboutUs = () => {
         </div>
       </div>
 
-      {/* LAYER 2 */}
-      <div className="h-screen bg-white">Layer 2 (Full Screen Height)</div>
+      {/* CREATING A PARENT DIV TO ACCOMMODATE THE MULTIPLE LAYERS */}
+      <div className="min-h-screen overflow-hidden">
+        {/* LAYER 2 */}
+        <div className="h-[150vh] bg-white relative">
+          {/* OVAL */}
+          <div className="absolute left-1/2 -translate-x-1/2 w-[150%] h-[100vh] bg-[#FF6C1F] rounded-[80%_80%] top-[20%]"></div>
+          <div className="absolute left-1/2 -translate-x-1/2 w-[250%] h-[500%] bg-[#FF6C1F] top-[70%]"></div>
+          {/* MISSION */}
+          <div className="font-leader z-10 text-center absolute right-2/3 translate-x-14 top-[40%] text-[50px] whitespace-nowrap">
+            Mission
+          </div>
+          {/* MISSION STATEMENT */}
+          <div className="font-content z-10 text-center absolute right-2/3 translate-x-48 top-[50%] text-[20px]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna.
+          </div>
+          {/* VISION */}
+          <div className="font-leader z-10 text-center absolute left-2/3 translate-x--14 top-[65%] text-[50px] whitespace-nowrap">
+            Vision
+          </div>
+          {/* VISION STATEMENT */}
+          <div className="font-content z-10 text-center absolute right-[70%] translate-x--148 top-[75%] text-[20px]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna.
+          </div>
+        </div>
+      </div>
+      {/* CREATING A PARENT DIV TO ACCOMMODATE THE MULTIPLE LAYERS */}
+      <div className="overflow-hidden">
+        {/* LAYER 3 */}
+        <div className="h-screen bg-white relative">
+          {/* OVAL */}
+          <div className="absolute left-1/2 -translate-x-1/2 w-[150%] h-[100vh] bg-[#FF6C1F] rounded-[80%_80%] top-[20%]"></div>
+        </div>
+      </div>
     </div>
   );
 };
