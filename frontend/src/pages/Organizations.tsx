9ae8f7236dfddbe441e6e78184fa3624392535bc
elsdave <elsdave.constantino@wvsu.edu.ph>
2024-11-23 07:46:51 +0800

    deno and swiperjs compatibility issues

--- frontend/src/pages/Organizations.tsx
+++ frontend/src/pages/Organizations.tsx
@@ -1,12 +1,45 @@
-import React from "npm:react";
+import React from "react";
 import Nav from "../components/Nav";
+import Swiper from "../components/University_Based_Carousel.tsx";
+
 const Organizations = () => {
-  return (
+return (
+  <div className="flex flex-col w-full">
+    {/* nav bar */}
     <div>
       <Nav />
-      <h1>Organizations!</h1>
     </div>
-  );
+    {/* block 1 */}
+    <div className="text-center min-h-full" style={ {backgroundColor: "#AEA433"}}>
+        <div className="mx-14 my-72 ">
+          <div>
+            <h1 className="font-extrabold text-6xl"> WVSU <br></br>ORGANIZATIONS</h1>
+          </div>
+          <div className= "mt-2">
+            <p>Come and delve into the galaxy of celestial organizations, and find the one destined for you. </p>
+          </div>
+      </div>
+    </div>
+    {/* block 2 */}
+    <div className="text-center justify-start mt-4 mb-96">
+      <div>
+        <h1 className="font-extrabold text-4xl">University-Based Organizations</h1>
+      </div>
+      <div>
+      {/* <Swiper/> */}
+      </div>
+    </div>
+    {/* block 3 */}
+    <div style={ {backgroundColor: "#AEA433"}}>
+      <div className="text-center justify-start mt-4 mb-96" >
+        <div>
+          <h1 className="font-extrabold text-4xl">College-Based Organizations</h1>
+        </div>
+      </div>
+    </div>
+    {/* footer */}
+  </div>
+);
 };
 
 export default Organizations;
